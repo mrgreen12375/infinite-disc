5e2df73bb93624bb7e6cfdf07d936b39ca5743b6
melissarubiio <melirubio0603@gmail.com>
2022-11-12 13:35:29 -0700

    Commit Handlebars Draft

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,1 +1,11 @@
-/*intiial*/
+module.exports = {
+  format_date: (date) => {
+    // Format date as MM/DD/YYYY
+    return date.toLocaleDateString();
+  },
+  format_amount: (amount) => {
+    // format large numbers with commas
+    return parseInt(amount).toLocaleString();
+  },
+ 
+};
